adf2dc9ef86b055894ada55e644224e2fed41d8b
JiriKrizek <jkrizek@gmail.com>
2013-02-10 19:14:24 +0100

    Added Operation object

---
+++ operation.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby
+# encoding: UTF-8
+
+class Operation
+  def initialize(date, operation, value, value_ep, value_ep_new, place)
+    @date = date
+    @operation = operation
+    @value = value
+    @value_ep = value_ep
+    @value_ep_new = value_ep_new
+    @place = place
+  end
+
+  def to_s
+    puts "Date: #{@date}\n\toperation: #{@operation}\n\tvalue: #{@value}\t value_ep: #{@value_ep}\t value_ep_new: #{@value_ep_new}\n\tplace: #{@place}"
+  end
+end
