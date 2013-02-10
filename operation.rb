2cab009a20f4251326bc124c6890f8ca5d6301d1
JiriKrizek <jkrizek@gmail.com>
2013-02-10 20:36:11 +0100

    Added time_ago_in_words to time output

--- operation.rb
+++ operation.rb
@@ -1,7 +1,11 @@
 #!/usr/bin/env ruby
 # encoding: UTF-8
 
+require 'rubygems'
+require 'action_view'
+
 class Operation
+include ActionView::Helpers::DateHelper
   def initialize(date, operation, value, value_ep, value_ep_new, place)
     @date = date
     @operation = operation
@@ -12,6 +16,6 @@ class Operation
   end
 
   def to_s
-    "Date: #{@date}\n\toperation: #{@operation}\n\tvalue: #{@value}\t value_ep: #{@value_ep}\t value_ep_new: #{@value_ep_new}\n\tplace: #{@place}"
+    "Date: #{@date} (#{time_ago_in_words(Time.parse(@date.to_s))} ago)\n\toperation: #{@operation}\n\tvalue: #{@value}\t value_ep: #{@value_ep}\t value_ep_new: #{@value_ep_new}\n\tplace: #{@place}"
   end
 end
